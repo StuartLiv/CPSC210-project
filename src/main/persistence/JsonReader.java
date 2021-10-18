c61ab8fff20058d46447f298b8a5a61dfb8df040
Stuart Livingstone <stuliv04@students.cs.ubc.ca>
2021-10-17 22:21:01 -0700

    Phase 2 Task 4: Project structure updated for json implementation

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,10 @@
+package persistence;
+
+import org.json.*;
+
+//Modeled after JsonReader in JsonSerializationDemo
+//Link: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+//A reader handler for the 3 data types to read from file
+public class JsonReader {
+}
---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,10 @@
+package persistence;
+
+import org.json.*;
+
+//Modeled after JsonWriter in JsonSerializationDemo
+//Link: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+//A writer handler for the 3 data types
+public class JsonWriter {
+}
